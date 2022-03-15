4413964f4df89d644b5aa0abcf96d282c25be714
luisparf <luispauloaraujoafonso@gmail.com>
2022-03-15 14:14:55 -0300

    refactored

---
+++ PCV/Casos de Teste - Trabalho 1 - PCV/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 15/03/2022 11:59:21 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: Trabalho 1 - PCV
--- PCV/main.py
+++ PCV/main.py
@@ -1,23 +1,9 @@
-from graph_tools import Graph
-
-# create a graph with four nodes and two edges
-g = Graph(directed=True)
-g.add_edge(1, 2)
-g.add_edge(2, 3)
-g.add_vertex(4)
-#print(g)
-
-# find the all shortest paths from vertex 1
-dist, prev = g.dijkstra(1)
-#print(dist)
-
-# generate BA graph with 100 vertices
-g = Graph(directed=False).create_graph('barabasi', 100)
-
-
 if __name__ == '__main__':
-    # check if all vertices are mutually connected
-    print(g.is_connected())
 
-    # compute the betweenness centrality of vertex 1
-   # print(g.betweenness(1))
+    lines = []
+    while (line := str(input())) != "EOF":
+        line = line.replace('\r', '')
+        lines.append(line)
+
+    del lines[0:5]
+    print(lines)
