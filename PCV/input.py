d0a8a98139e161a4bf674920ec4c51ba5ee88ade
luisparf <luispauloaraujoafonso@gmail.com>
2022-03-16 11:12:24 -0300

    refactored...

--- PCV/input.py
+++ PCV/input.py
@@ -17,8 +17,7 @@ def fileinput():
 
 ############################################ RUN.CODES INPUT ##############################################
 
-def runinput():
-
+def runcodesinput():
     lines = []
     while (line := str(input())) != "EOF":
         line = line.replace('\r', '').replace('.', '').split()
@@ -26,6 +25,33 @@ def runinput():
 
     del lines[0:5]
 
+    lines = formatnode(lines)
     return lines
 
+
 ############################################################################################################
+
+def formatnode(lines):
+    dict = {}
+
+    for i in range(1, len(lines)):
+        # dict["node"] = lines[i][0]  # sei lá se usaremos...
+        dict["x"] = lines[i][1]
+        dict["y"] = lines[i][2]
+
+        lines[i] = dict.copy()
+        # print('\n'.join("{}: {}".format(k, v) for k, v in lines[i].items()))
+
+        '''
+        x1 = int(lines[1]['x'])
+        y1 = int(lines[1]['y'])
+
+        x2 = int(lines[i]['x'])
+        y2 = int(lines[i]['y'])
+
+        a = [x1, y1]
+        b = [x2, y2]
+        # print("Euclidian distance between node {}{} and node {}{} is {}\n".format(1, a, i, b, distance.euclidean(a, b)))
+        # print("Euclidian distance between node {}{} and node {}{} is {}\n".format(1, a, i, b, dist(a, b)))
+        '''
+    return lines
--- PCV/main.py
+++ PCV/main.py
@@ -1,27 +1,7 @@
 # from scipy.spatial import distance
-from input import fileinput, runinput
-from math import dist
+from input import fileinput, runcodesinput
+
 if __name__ == '__main__':
-
     # lines = fileinput()
-    lines = runinput()
-    dict = {}
-
-    for i in range(1, len(lines)):
-        dict["node"] = lines[i][0]  # sei lá se usaremos...
-        dict["x"] = lines[i][1]  # same
-        dict["y"] = lines[i][2]  # same ^
-
-        lines[i] = dict.copy()
-        print('\n'.join("{}: {}".format(k, v) for k, v in lines[i].items()))
-
-        x1 = int(lines[1]['x'])
-        y1 = int(lines[1]['y'])
-
-        x2 = int(lines[i]['x'])
-        y2 = int(lines[i]['y'])
-
-        a = (x1, y1)
-        b = (x2, y2)
-        # print("Euclidian distance between node {}{} and node {}{} is {}\n".format(1, a, i, b, distance.euclidean(a, b)))
-        print("Euclidian distance between node {}{} and node {}{} is {}\n".format(1, a, i, b, dist(a, b)))
+    nodes = runcodesinput()  # lê o arquivo e armazena cada nó em uma lista, onde nó i está no indice i da lista e armazena suas coordenadas x,y
+    print(nodes)
