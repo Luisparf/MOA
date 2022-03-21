46341e3690ff47ff4c8fddf83a228389fb642374
Luisparf <luispauloaraujoafonso@gmail.com>
2022-03-21 13:16:04 -0300

    first subrote of G

--- PCV/constructive.py
+++ PCV/constructive.py
@@ -7,51 +7,52 @@
 
 from math import dist
 from random import randint
-
+from functools import reduce
 
 # Uma cópia local de funções como essa reduz o tempo de execução
 localLen = len
+localMax = max
 
-def nodesDistances(nodes): # armazena todas as distâncias  nó X nó
-    distances = {}
-    for i in range(1, len(nodes)):
+def getAllDistances(graph): # armazena todas as distâncias  nó X nó
+    allDistances = {}
+    for i in range(1, len(graph)):
         try:
-            distances[i]
+            allDistances[i]
         except:
-            distances[i] = {}
+            allDistances[i] = {}
 
-        distances[i][i] = 0.0
-        for a in range(i+1, len(nodes)):            
+        allDistances[i][i] = 0.0
+        for a in range(i+1, len(graph)):            
             try:
-                distances[a]
+                allDistances[a]
             except:
-                distances[a] = {}
+                allDistances[a] = {}
 
             
-            x0 = nodes[i]['x']
-            y0 = nodes[i]['y']
+            x0 = graph[i]['x']
+            y0 = graph[i]['y']
             
-            x1 = nodes[a]['x']
-            y1 = nodes[a]['y']
+            x1 = graph[a]['x']
+            y1 = graph[a]['y']
             
 
             calculatedDist = int(dist([x0, y0], [x1, y1])) # Só considerando parte inteira
             #calculatedDist = dist([x0, y0], [x1, y1]) # Considerando ponto flutuante
 
-            distances[i][a] = calculatedDist
-            distances[a][i] = calculatedDist
+            allDistances[i][a] = calculatedDist
+            allDistances[a][i] = calculatedDist
 
-    return distances
+    return allDistances
 
 ###########################################################################################
 
-def matrizConstrutive(nodes, distances):
-    # selected = randint(1, localLen(nodes)-1)
-    # first = selected
+def matrizConstrutive(graph, allDistances):
+    selected = randint(1, localLen(graph)-1)
+    first = selected
 
     walkWeight = 0
     walkedPath = []
-    walkedPath.append(nodes[selected])
+    walkedPath.append(graph[selected])
 
     while True:
         # Valor da distância entre nó atual e menor vizinho
@@ -62,24 +63,24 @@ def matrizConstrutive(nodes, distances):
         
         # Conteiro para verificar se todos os vizinho já foram explorados 
         endCounter = 0
-        for i in range(1, localLen(nodes)):
+        for i in range(1, localLen(graph)):
             # print("selected = {} i = {}".format(selected, i))
 
-            if (i == selected) or (nodes[i]['used']):
+            if (i == selected) or (graph[i]['used']):
                 endCounter += 1
                 continue
 
-            if distances[selected][i] < menor:
-                menor = distances[selected][i]
+            if allDistances[selected][i] < menor:
+                menor = allDistances[selected][i]
                 menorIndex = i
         
-        if endCounter == (localLen(nodes) - 2):
+        if endCounter == (localLen(graph) - 2):
             walkedPath.append(first)
             break
 
         walkWeight += menor
         walkedPath.append(menorIndex)
-        nodes[menorIndex]['used'] = True
+        graph[menorIndex]['used'] = True
         selected = menorIndex
 
     
@@ -89,5 +90,81 @@ def matrizConstrutive(nodes, distances):
 
 ###########################################################################################
 
-def insertDistant(nodes):
+def get_key(allDistances,val):
+    """
+    função que recupera o indide do vértice com a maior distancia para o vértice selecionado
+    """
+    for key, value in allDistances.items():
+        if val == value:
+             return key
+ 
+    return "key doesn't exist"
+
+############################################################################################
+
+def printGraph(graph):
+    for i in range(1, localLen(graph)):
+        print(f"\n{i}")
+        print('\n'.join("{}: {}".format(k, v) for k, v in graph[i].items()))
+   
+############################################################################################
+
+def findMax(graph, allDistances, subRote, i):
+    greaterDistance = 0
+    for j in range(1, localLen(graph)):
+        if allDistances[i][j] > greaterDistance and graph[j]['used'] == False:
+            greaterDistance = allDistances[i][j]
+            greaterIndex = j
+
+    graph[greaterIndex]['used'] = True
+    subRote.append(greaterIndex)
+
+    print("i = {} , j  = {} , Cij = {} ".format(i, greaterIndex, greaterDistance))
+    print('Subrote = {}'.format(subRote))
     print()
+    # print("All distances from {}: {}".format(i,allDistances[i]))
+
+
+############################################################################################
+
+def insertMoreDistant(graph, allDistances):
+
+    """
+    Algorithm source:
+
+    XXIV Encontro Nac. de Eng. de Produção - Florianópolis, SC, Brasil, 03 a 05 de nov de 2004
+
+    Análise Comparativa de Algoritmos Heurísticos para Resolução do
+    Problema do Caixeiro-Viajante em Grafos Não Clusterizados
+
+    link:http://abepro.org.br/biblioteca/ENEGEP2004_Enegep0601_0567.pdf
+
+    """
+    
+
+    subRote = []
+    
+    # 1. Comece com um subgrafo composto apenas pelo i-ésimo vértice;
+    i = randint(1, localLen(graph)-1)
+    graph[i]['used'] = True
+    subRote.append(i)
+
+    # 2. Encontre um vértice j ≠ i tal que cij seja máximo e forme a subrota i − j − i ;
+    # distance = localMax(allDistances[i].values())
+    findMax(graph,allDistances,subRote,i)
+
+
+    # 3. Dada uma subrota, encontre um vértice j que não pertence a esta subrota, 
+    # mas que seja o mais distante de todos os vértices pertencentes à mesma;
+    i = randint(1, localLen(subRote)-1) # escolhe um vértice da subrota para escolher o vértice mais distante
+    i = subRote[i]
+    print('Chosen vertex to find max distance: {}'.format(i))
+
+    findMax(graph,allDistances,subRote, i)
+
+   
+    # print("All distances from {}: {}".format(i,allDistances[i]))
+
+
+
+   
--- PCV/main.py
+++ PCV/main.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from input import fileinput, runcodesinput
-from constructive import matrizConstrutive, nodesDistances, insertDistant
+from constructive import matrizConstrutive, getAllDistances, insertMoreDistant, printGraph
 
 if __name__ == '__main__':
 
     # lines = fileinput()
-    nodes = runcodesinput()  # lê o arquivo e armazena cada nó em uma lista, onde cada nó i está no indice i da lista e contém suas coordenadas x,y
-    distances = nodesDistances(nodes) # distâncias de nó para nó
-    
+    graph = runcodesinput()  # lê o arquivo e armazena cada nó em uma lista, onde cada nó i está no indice i da lista e contém suas coordenadas x,y
+    allDistances = getAllDistances(graph) # distâncias de nó para nó
 
+    insertMoreDistant(graph, allDistances)
+    # printGraph(graph)
     
-
-    # print(distances[24]) 
-    # print(distances[1][3]) # distancia entre o nó 1 e nó 3
+     # distancia entre o nó 1 e nó 3
     # Printar resultado da heurística vizinho mais próximo (considerando só a parte inteira)
     # print(int(matrizConstrutive(nodes, distances)))
