85ad2a3f776d146a9d22364200f366f1dcf90264
Vitor Cecilio <vtr.cecilio@gmail.com>
2022-03-28 10:34:37 -0300

    nearestNeighbour arrumado

--- PCV/main.py
+++ PCV/main.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from input import fileinput, runcodesinput
-from constructive import matrizConstrutive, getAllDistances, insertMoreDistant, printGraph
+from constructive import nearestNeighbour, getAllDistances, insertMoreDistant, printGraph
 from improvement import two_opt
 
 if __name__ == '__main__':
 
     ### Input ###
     graph = runcodesinput()  # lê o arquivo e armazena cada nó em uma lista, onde cada nó i está no indice i da lista e contém suas coordenadas x,y
-    # graph = fileinput()
+    graph = fileinput()
     allDistances = getAllDistances(graph) # distâncias de nó para nó
-    printGraph(graph)
+    #printGraph(graph)
 
     ### Heurística construtiva Inserção do mais distante
-    graph = insertMoreDistant(graph, allDistances)
-    print(graph)
+    #graph = insertMoreDistant(graph, allDistances)
+    #print(graph)
     
     ### Heurística melhorativa 2-opt
-    print(int(two_opt(allDistances, graph)))
+    #print(int(two_opt(allDistances, graph)))
 
    
 
     # Printar resultado da heurística vizinho mais próximo (considerando só a parte inteira)
-    # print(int(matrizConstrutive(nodes, distances)))
+    print(int(nearestNeighbour(graph, allDistances)))
