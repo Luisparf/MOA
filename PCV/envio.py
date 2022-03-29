1b2c8d8a781c6495d620b6bcf8a3b653cbbef003
Vitor Cecilio <vtr.cecilio@gmail.com>
2022-03-28 23:00:04 -0300

    new

--- PCV/envio.py
+++ PCV/envio.py
@@ -1,16 +1,95 @@
 # -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
 ###########################################################################################
 #                                                                                         #
 #                Módulo que contém os algoritmos construtivos para o PCV                  #                          
 #                                                                                         #      
 ###########################################################################################
 
-from math import dist
-from random import randint
-from improvement import sumDistance
-
 # Uma cópia local de funções como essa reduz o tempo de execução
 localLen = len
+
+# -*- coding: utf-8 -*-
+###########################################################################################
+#                                                                                         #
+#                Módulo que contém as funções para entrada e formatação dos dados         #                          
+#                                                                                         #      
+###########################################################################################
+
+################################### INPUT BY FILE #########################################
+
+from math import dist
+from random import randint
+
+def fileinput():
+    """
+
+    Apenas para uma possível versão futura...
+
+    """
+    f = open('1.in', 'r')
+    d = {}
+    lines = f.readlines()
+
+    del lines[0:5]
+    lines.pop()
+
+    for i in range(1, len(lines)):
+        line = lines[i].replace('\r', '').replace('.', '').split()
+        lines[i] = line    
+
+    for i in range(1, len(lines)):
+        d["used"] = False
+        d["x"] = float(lines[i][1])
+        d["y"] = float(lines[i][2])
+
+
+        lines[i] = d.copy()
+
+    f.close()
+    return lines
+
+
+################################## RUN.CODES INPUT ########################################
+
+def runcodesinput():
+    lines = []
+    d = {}
+    
+    while True:
+        try:
+            line = str(input())
+            if line == "EOF":
+                break
+        except EOFError : 
+            break
+        
+        line = line.replace('\r', '').split() 
+        lines.append(line.copy())
+
+    del lines[0:5]
+
+    for i in range(1, len(lines)):
+
+        try:
+            d["used"] = False
+            d["x"] = float(lines[i][1])
+            d["y"] = float(lines[i][2])
+            
+        except IndexError:
+            # lines[i] = d.copy()
+            break
+
+        lines[i] = d.copy()
+
+    # lines.append(lines[1])  
+    # print(lines)  
+
+    return lines
+
+
+
 
 
 def getAllDistances(graph): # armazena todas as distâncias  nó X nó
@@ -25,7 +104,7 @@ def getAllDistances(graph): # armazena todas as distâncias  nó X nó
         allDistances[i][i] = 0.0
         x0 = graph[i]['x']
         y0 = graph[i]['y']
-        for a in range(i+1, localLen(graph)):  
+        for a in range(i+1, len(graph)):  
             #print(i, a)
 
             try:
@@ -213,13 +292,126 @@ def insertMoreDistant(graph, dist):
 
     
     
+# -*- coding: utf-8 -*-
+###########################################################################################
+#                                                                                         #
+#                Módulo que contém os algoritmos nelhorativos para o PCV                  #                          
+#                                                                                         #      
+###########################################################################################
 
+localLen = len
 
-    
+def sumDistance(dist, route):
 
-    
+	sizeroute = localLen(route)-1
+	walkWeight = 0
+	for i in range(sizeroute):
+		walkWeight += dist[route[i]][route[i+1]]
+        # 	print("route[{}] = {}".format(i,route[i]))
+
+	return walkWeight
+
+############################################################################################
+
+def two_opt_swap(route, i, k):
+
+	newroute = []
+	sizeroute = localLen(route)
+
+	newroute.append(route[0:i])
+	newroute.append(route[k-2:i-1:-1])
+	newroute.append(route[k-1:sizeroute])
+
+	flatroute = [item for sublist in newroute for item in sublist]
+	# print("\nflatroute = {} i = {} k = {}".format(flatroute, i, k))
+	return flatroute
+
+############################################################################################
+
+def two_opt(dist, route):
+	"""
+
+    2-opt heuristic.
+
+    Algorithm pseudo-code:
+
+	2optSwap(route, i, k) {
+	     1. take route[1] to route[i-1] and add them in order to new_route
+	     2. take route[i] to route[k] and add them in reverse order to new_route
+	     3. take route[k+1] to end and add them in order to new_route
+	     return new_route;
+	}
+
+	
+	repeat until no improvement is made {
+       start_again:
+       best_distance = calculateTotalDistance(existing_route)
+       for (i = 0; i < number of nodes eligible to be swapped - 1; i++) {
+           for (k = i + 1; k < number of nodes eligible to be swapped; k++) {
+               new_route = 2optSwap(existing_route, i, k)
+               new_distance = calculateTotalDistance(new_route)
+               if (new_distance < best_distance) {
+                   existing_route = new_route
+                   goto start_again
+	            }
+	        }
+	    }
+	 }
+
+    link: https://en.wikipedia.org/wiki/2-opt
+
+    """
+
+	sizeRoute = localLen(route)
+	bestRoute = route
+	improved = True
+
+	while improved:
+
+		improved = False
+
+		for i in range(1, sizeRoute - 2):
+			bestDistance = sumDistance(dist, route)
+
+			for j in range(i+1, sizeRoute):
+				# newRoute = two_opt_swap(route.copy(), i, k)
+				newRoute = route[:]
+				newRoute[i:j] = route[j-1:i-1:-1] # o mesmo que two_opt_swap
+				# print(newRoute)
+				newDistance = sumDistance(dist, newRoute)
+
+				if newDistance < bestDistance:
+					bestRoute = newRoute
+					improved = True
+					bestDistance = newDistance
+
+			route = bestRoute	
+	#print()
+	#print(route)
+	return bestDistance
+
 
 
 
 
    
+
+if __name__ == '__main__':
+
+    ### Input ###
+    graph = runcodesinput()  # lê o arquivo e armazena cada nó em uma lista, onde cada nó i está no indice i da lista e contém suas coordenadas x,y
+    allDistances = getAllDistances(graph) # distâncias de nó para nó
+    # print(allDistances)
+    
+    ### Heurística construtiva Inserção do mais distante
+    #graph = insertMoreDistant(graph, allDistances)
+    
+
+    ### Heurística melhorativa 2-opt
+    #print(int(two_opt(allDistances, graph)))
+
+   
+
+    # Printar resultado da heurística vizinho mais próximo (considerando só a parte inteira)
+    graph = nearestNeighbour(graph, allDistances)
+    print(int(two_opt(allDistances, graph)))
