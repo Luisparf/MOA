3d162de2fef312141f6ed958249fbfb4ef1aef52
luisparf <luispauloaraujoafonso@gmail.com>
2022-05-07 21:18:14 -0300

    algoritmo implementando, falta corrigir bugs e os testes

--- PCV/genetico/genetic.py
+++ PCV/genetico/genetic.py
@@ -33,6 +33,19 @@ def fitness(graph, route):
 
 ###########################################################################################
 
+def search_worst_fit(population, graph):
+
+    worst = 0
+    index = 0
+    for i in range(localLen(population) - 1):
+        atual = fitness(graph, population[i])
+        if atual > worst:
+            worst = atual
+            index = i
+    return worst, index
+
+###########################################################################################
+
 def nearestneighbour(graph, j):
     # selected = randint(1, localLen(graph)-1)
     selected = j
@@ -75,6 +88,91 @@ def nearestneighbour(graph, j):
 
 
 ###########################################################################################
+
+def two_opt(graph, route, x):
+    """
+    2-opt heuristic.
+    Algorithm pseudo-code:
+    2optSwap(route, i, k) {
+         1. take route[1] to route[i-1] and add them in order to new_route
+         2. take route[i] to route[k] and add them in reverse order to new_route
+         3. take route[k+1] to end and add them in order to new_route
+         return new_route;
+    }
+    repeat until no improvement is made {
+       start_again:
+       best_distance = calculateTotalDistance(existing_route)
+       for (i = 0; i < number of nodes eligible to be swapped - 1; i++) {
+           for (k = i + 1; k < number of nodes eligible to be swapped; k++) {
+               new_route = 2optSwap(existing_route, i, k)
+               new_distance = calculateTotalDistance(new_route)
+               if (new_distance < best_distance) {
+                   existing_route = new_route
+                   goto start_again
+                }
+            }
+        }
+     }
+    link: https://en.wikipedia.org/wiki/2-opt
+    """
+
+    size_route = localLen(route)
+    best_route = route
+    improved = True
+
+    plt_counter = -1
+    plt_counters = []
+    plt_opts = []
+
+    counter = 0
+    while improved:
+        should_break = False
+
+        improved = False
+        best_distance = fitness(graph, route)
+        for i in range(1, size_route - 2):
+            if should_break:
+                break
+            for j in range(i + 1, size_route):
+                # newRoute = two_opt_swap(route.copy(), i, k)
+                new_route = route[:]
+                new_route[i:j] = route[j - 1:i - 1:-1]  # o mesmo que two_opt_swap
+                # print(newRoute)
+                new_distance = fitness(graph, new_route)
+
+                ### trecho para matplot ###
+                plt_opts.append(best_distance)
+                plt_counter += 1
+                plt_counters.append(plt_counter)
+                #################
+
+                if new_distance < best_distance:
+                    best_route = new_route
+                    improved = True
+                    best_distance = new_distance
+                    counter += 1
+                    """
+                    ### trecho para matplot ###
+                    plt_opts.append(best_distance)
+                    plt_counter += 1
+                    plt_counters.append(plt_counter)
+                    #################
+                    """
+                if x == 1 and counter >= 1: # critério de parada, first improvement
+                    should_break = True
+                    improved = False
+                    break
+
+            route = best_route
+            if should_break:
+                break
+            # print()
+    # print(counter)
+    # print(route)
+    return route #, plt_counters, plt_opts
+
+###########################################################################################
+
 
 def pmx(f1, f2):
     s1 = []  # filho 1
@@ -148,32 +246,49 @@ def selection(graph, population, k, s):  # seleção por torneio
 
 ###########################################################################################
 
-def mutation(mut, cromossomos, pop):
+def mutation(mut, population, pop):
     swap_list = []
     mut = int(mut)
-    mut_tax = floor((mut / 100) * localLen(cromossomos[1]))
-    # print(f'mut_tax:{mut_tax}')
+    mut_tax = floor((mut / 100) * localLen(population[1]))
+    print(f'mut:{mut} mut_tax:{mut_tax}')
 
     for i in range(mut_tax):
-        swap_list.append(randint(i, pop - 1))
+        swap_list.append(randint(i, pop - 2))
 
-    i = (randint(1, localLen(cromossomos[1]) - 1))
-    j = (randint(i, localLen(cromossomos[1]) - 1))
+    i = (randint(1, localLen(population[1]) - 1))
+    j = (randint(i, localLen(population[1]) - 1))
 
+    print(f'swaplist:{swap_list} i:{i} j:{j}')
     for index in swap_list:
-        aux = cromossomos[index][j]
-        cromossomos[index][j] = cromossomos[index][i]
-        cromossomos[index][i] = aux
+        aux = population[index][j]
+        population[index][j] = population[index][i]
+        population[index][i] = aux
 
-    return cromossomos
+    return population
 
 
 ###########################################################################################
 
+def steady_stated(graph, population, filhos):
+    worst, worst_index = search_worst_fit(population, graph)
+
+    menor = filhos[1]
+    for i in range(2, localLen(filhos)):
+        if fitness(graph, filhos[i]) < fitness(graph, menor):
+            menor = filhos[i]
+
+    if fitness(graph, menor) < worst:
+        population[worst_index] = menor
+
+    return  population
+
+###########################################################################################
 
 def genetic(graph, pop, mut, max_i, max_t, s):
     population = []
+    busca = []
     k = 4
+    it = 0
 
     if pop > localLen(graph) - 1:
         pop = localLen(graph)
@@ -186,29 +301,49 @@ def genetic(graph, pop, mut, max_i, max_t, s):
         print(f'elemento:{elemento}')
 
     start_time = time.time()
-    # while time.time() - start_time  <= max_t:
+    while time.time() - start_time <= max_t or it == max_i:
 
-    ### Avaliação ###
+        ### Avaliação ###
 
-    #### Seleção ###
-    pais = selection(graph, population, k, s)
-    print(f'\nSeleção:\n')
-    for pai in pais:
-        print(f'{pai}\n')
+        #### Seleção ###
+        pais = selection(graph, population, k, s)
+        print(f'\nSeleção:')
+        for pai in pais:
+            print(f'{pai}\n')
 
-    ### Cruzamento ###
-    print(f'\nCruzamento:\n')
-    filhos = cx(pais)
-    for filho in filhos:
-        print(f'{filho} - tamanho:{localLen(filho)}\n')
+        ### Cruzamento ###
+        print(f'\nCruzamento:')
+        filhos = cx(pais)
+        for filho in filhos:
+            print(f'{filho} - tamanho:{localLen(filho)}\n')
 
-    ### Mutação ###
-    print(f'\nMutação:\n')
-    cromossomos = mutation(mut, population, pop)
-    for cromossomo in cromossomos:
-        print(f'{cromossomo} - tamanho:{localLen(cromossomo)}\n')
+        ### Mutação ###
+        print(f'\nMutação:')
+        cromossomos = mutation(mut, population, pop)
+        for cromossomo in cromossomos:
+           print(f'{cromossomo} - tamanho:{localLen(cromossomo)}\n')
 
-    ### Busca Local ###
+        ### Busca Local ###
+        print(f'\nBusca Local:')
+        for filho in filhos:
+            busca.append(two_opt(graph, filho, 1))
+
+        for elemento in busca:
+            print(f'{elemento}\n')
+
+        for elemento in population:
+            print(f'elemento:{elemento}')
+
+        print(f'\n\n\n')
+        ### atualização ###
+        population = steady_stated(graph, population, filhos)
+
+        for elemento in population:
+            print(f'elemento:{elemento}')
+
+        i += 1
+
+    print(population)
 
 
-    ### atualização ###
+
--- PCV/genetico/main.py
+++ PCV/genetico/main.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     ### Input ###
     args, graph = fileinput()
 
-    genetic(graph[:], args.population, args.mutation, args.max_iteration, args.max_time * 60, args.seed)
+    genetic(graph[:], args.population, args.mutation, int(args.max_iteration), int(args.max_time * 60) , args.seed)
     
     
 
